af5670a679c2b1e7195bc8f1f5db7b48104de52c
den-ii <wisdomochiche@gmail.com>
2025-02-06 15:11:40 +0100

    bj svg

--- Swaply/components/CountrySVG.tsx
+++ Swaply/components/CountrySVG.tsx
@@ -1,6 +1,9 @@
 import NGN_32 from "@/assets/images/NGN_32.svg";
 import NGN_16 from "@/assets/images/NGN_16.svg";
 import NGN_20 from "@/assets/images/NGN_20.svg";
+import CFA_32 from "@/assets/images/CFA_32.svg";
+import CFA_16 from "@/assets/images/CFA_16.svg";
+import CFA_20 from "@/assets/images/CFA_20.svg";
 
 export default function CountrySVG({
   country,
@@ -9,7 +12,7 @@ export default function CountrySVG({
   country: string;
   size: number;
 }) {
-  switch (country) {
+  switch (country.toUpperCase()) {
     case "NG":
       if (size === 32) {
         return <NGN_32 />;
@@ -17,5 +20,12 @@ export default function CountrySVG({
         return <NGN_16 />;
       }
       return <NGN_20 />;
+    case "BJ":
+      if (size === 32) {
+        return <CFA_32 />;
+      } else if (size === 16) {
+        return <CFA_16 />;
+      }
+      return <CFA_20 />;
   }
 }
